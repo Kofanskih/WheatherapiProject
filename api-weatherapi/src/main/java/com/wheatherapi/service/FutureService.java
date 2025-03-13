b9b0742832fe8c738db4dcf2ca8f2346d8aa534e
Kofanskih <kofanskih08@gmail.com>
2025-03-13 23:27:16 +0200

    Changed FutureWeather, FutureService

--- api-weatherapi/src/main/java/com/wheatherapi/service/FutureService.java
+++ api-weatherapi/src/main/java/com/wheatherapi/service/FutureService.java
@@ -2,7 +2,10 @@ package com.wheatherapi.service;
 
 import com.wheatherapi.AssertableResponse;
 import com.wheatherapi.models.queryParameters.FutureWeatherRequest.FutureWeatherQueryParams;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
 import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
 import utils.GetApiKey;
 import utils.GetPath;
 
@@ -10,6 +13,18 @@ import java.io.IOException;
 
 public class FutureService extends BaseService{
     private final String FUTURE_PATH = "/future.json?q=%s&dt=%s&key=%s";
+    private final String REQUIRED_Q = "q";
+    private final String REQUIRED_DATA = "dt";
+    private final String APIKEY = "key";
+
+    protected RequestSpecification baseConfigurationRestAssuredFuture(String q, String data, String apiKey) throws IOException {
+        return baseConfigurationRestAssured()
+                .basePath(FUTURE_PATH)
+                .params(REQUIRED_Q, q, REQUIRED_DATA, data, APIKEY, apiKey)
+                .when()
+                .filters(new RequestLoggingFilter(), new ResponseLoggingFilter());
+
+    }
 
     public Response sendFutureRequest(String path, String City, String data, String apiKey) throws IOException {
         return baseConfigurationRestAssured()
@@ -17,14 +32,14 @@ public class FutureService extends BaseService{
 
     }
 
-    public Response sendFutureRequest2(FutureWeatherQueryParams futureWeatherQueryParams) throws IOException {
-        return baseConfigurationRestAssuredFuture(futureWeatherQueryParams.getQ(), futureWeatherQueryParams.getKey(), futureWeatherQueryParams.getDt())
-                .get();
+    public Response sendFutureRequest2(String path, FutureWeatherQueryParams futureWeatherQueryParams, String apiKey) throws IOException {
+        return baseConfigurationRestAssured()
+                .get(String.format(new GetPath().takePath(path),futureWeatherQueryParams.getQ(), futureWeatherQueryParams.getDt(), new GetApiKey().takeApiKey(apiKey)));
 
     }
 
     public AssertableResponse sendFutureRequest3(FutureWeatherQueryParams futureWeatherQueryParams) throws IOException {
-        Response response = baseConfigurationRestAssuredFuture(futureWeatherQueryParams.getQ(), futureWeatherQueryParams.getKey(), futureWeatherQueryParams.getDt())
+        Response response = baseConfigurationRestAssuredFuture(futureWeatherQueryParams.getQ(), futureWeatherQueryParams.getDt(), futureWeatherQueryParams.getKey())
                 .get().then().extract().response();
 
         return new AssertableResponse(response);
--- api-weatherapi/src/test/java/FutureWeatherTest.java
+++ api-weatherapi/src/test/java/FutureWeatherTest.java
@@ -1,6 +1,5 @@
 import com.wheatherapi.models.queryParameters.FutureWeatherRequest.FutureWeatherQueryParams;
 import com.wheatherapi.models.queryParameters.FutureWeatherResponse.FutureWeatherResponse;
-import com.wheatherapi.models.queryParameters.FutureWeatherResponse.Location;
 import com.wheatherapi.service.FutureService;
 import org.hamcrest.Matchers;
 import org.testng.Assert;
@@ -22,23 +21,6 @@ public class FutureWeatherTest {
                 .body("forecast.forecastday.day.condition.text", Matchers.notNullValue());
     }
 
-    @Test
-    public void verifyDayConditionIsNotNull1() throws IOException {
-        new FutureService()
-                .sendFutureRequest("future_path","Malaga", "2025-04-01", "api_key")
-                .then()
-                .statusCode(200)
-                .body("forecast.forecastday.day.condition.text", Matchers.notNullValue());
-    }
-
-    @Test
-    public void verifyDayConditionIsNotNull2() throws IOException {
-        new FutureService()
-                .sendFutureRequest4("Malaga", "2025-04-01", "api_key")
-                .then()
-                .statusCode(200)
-                .body("forecast.forecastday.day.condition.text", Matchers.notNullValue());
-    }
 
 
     @Test
@@ -46,14 +28,14 @@ public class FutureWeatherTest {
         FutureWeatherQueryParams futureWeatherQueryParams = new FutureWeatherQueryParams();
         futureWeatherQueryParams.setQ("Malaga");
         futureWeatherQueryParams.setDt("2025-04-01");
-        futureWeatherQueryParams.setKey("076542489b7542299d1200203250703");
-        new FutureService()
-                .sendFutureRequest2(futureWeatherQueryParams)
+        FutureWeatherResponse futureWeatherResponse = new FutureService()
+                .sendFutureRequest2("future_path", futureWeatherQueryParams, "api_key")
                 .then()
                 .statusCode(200)
                 .extract()
                 .response()
-                .as(FutureWeatherQueryParams.class);
+                .as(FutureWeatherResponse.class);
+        Assert.assertNotNull(futureWeatherResponse.getLocation().getName(), "Token is null");
 
     }
 
@@ -94,5 +76,14 @@ public class FutureWeatherTest {
                 .shouldHave(statusCode(200)).responseAs(FutureWeatherResponse.class);
 
         Assert.assertNotNull(futureWeatherResponse.getLocation().getName(), "Token is null");
+    }
+
+    @Test
+    public void verifyDayConditionIsNotNull2() throws IOException {
+        new FutureService()
+                .sendFutureRequest4("Malaga", "2025-04-01", "api_key")
+                .then()
+                .statusCode(200)
+                .body("forecast.forecastday.day.condition.text", Matchers.notNullValue());
     }
 }
