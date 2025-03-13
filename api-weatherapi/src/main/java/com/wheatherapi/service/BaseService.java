8d5fa8dbd48ea4851af2b5a09c4e9cf9ed0b39b9
Kofanskih <kofanskih08@gmail.com>
2025-03-13 23:14:22 +0200

    Changed paths.properties, BaseService

--- api-weatherapi/src/main/java/com/wheatherapi/service/BaseService.java
+++ api-weatherapi/src/main/java/com/wheatherapi/service/BaseService.java
@@ -11,10 +11,6 @@ import java.io.IOException;
 import static io.restassured.RestAssured.given;
 
 public class BaseService {
-    private final String FUTURE_PATH = "/future.json?";
-    private final String REQUIRED_Q = "q";
-    private final String REQUIRED_DATA = "dt";
-    private final String APIKEY = "key";
 
     protected RequestSpecification baseConfigurationRestAssured() throws IOException {
         RequestSpecification reqSpec = given()
@@ -25,18 +21,4 @@ public class BaseService {
         return reqSpec;
 
     }
-
-    protected RequestSpecification baseConfigurationRestAssuredFuture(String q, String apiKey, String data) throws IOException {
-        RequestSpecification reqSpec = given()
-                .contentType(ContentType.JSON)
-                .baseUri(new GetMainURL().takeMainURL("main_url"))
-                .basePath(FUTURE_PATH)
-                .params(REQUIRED_Q, q, APIKEY, apiKey, REQUIRED_DATA, data)
-                .when()
-                .filters(new RequestLoggingFilter(), new ResponseLoggingFilter());
-        return reqSpec;
-
-    }
-
-
 }
