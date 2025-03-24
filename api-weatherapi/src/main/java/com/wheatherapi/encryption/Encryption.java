3d0e92b7d094239070b77c071dba36b211df077b
Kofanskih <kofanskih08@gmail.com>
2025-03-24 14:38:41 +0200

    Update encryption

--- api-weatherapi/src/main/java/com/wheatherapi/encryption/Encryption.java
+++ api-weatherapi/src/main/java/com/wheatherapi/encryption/Encryption.java
@@ -10,33 +10,9 @@ import java.util.Base64;
 
 public class Encryption {
     public static void main(String[] args) throws Exception {
-        // Генерация ключа для шифрования
-        KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
-        keyGenerator.init(128); // Указываем размер ключа
-        SecretKey secretKey = keyGenerator.generateKey();
-
-        // Создание экземпляра Cipher для алгоритма AES
-        Cipher cipher = Cipher.getInstance("AES");
-
-        // Инициализация Cipher в режиме шифрования с нашим ключом
-        cipher.init(Cipher.ENCRYPT_MODE, secretKey);
-        String secretMessage = "Это секретное сообщение";
-        byte[] encryptedMessage = cipher.doFinal(secretMessage.getBytes());
-
-        // Преобразование зашифрованного сообщения в строку для отправки
-        String encodedMessage = Base64.getEncoder().encodeToString(encryptedMessage);
-        System.out.println("Зашифрованное сообщение: " + encodedMessage);
-
-        // Инициализация Cipher в режиме дешифрования
-        cipher.init(Cipher.DECRYPT_MODE, secretKey);
-        byte[] decryptedBytes = cipher.doFinal(Base64.getDecoder().decode(encodedMessage));
-        String decryptedMessage = new String(decryptedBytes);
-        System.out.println("Расшифрованное сообщение: " + decryptedMessage);
-
-        System.out.println();
-
-        //encryptApi();
-        decryptApi(keyGeneration(), encryptApi());
+        SecretKey secretKey1 = keyGeneration();
+        String encryptApi = encryptApi(secretKey1);
+        decryptApi(secretKey1, encryptApi);
     }
 
 
@@ -49,9 +25,8 @@ public class Encryption {
         return secretKey;
     }
 
-    public static String encryptApi() throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException, IOException {
+    public static String encryptApi(SecretKey secretKey) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException, IOException {
         Cipher cipher = Cipher.getInstance("AES");
-        SecretKey secretKey = keyGeneration();
 
         cipher.init(Cipher.ENCRYPT_MODE, secretKey);
         String secretApi = "076542489b7542299d1200203250703";
@@ -64,9 +39,11 @@ public class Encryption {
 
     public static String decryptApi(SecretKey secretKey, String encodedMessage) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException, IOException {
         Cipher cipher = Cipher.getInstance("AES");
+
         cipher.init(Cipher.DECRYPT_MODE, secretKey);
         byte[] decryptedBytes = cipher.doFinal(Base64.getDecoder().decode(encodedMessage));
         String decryptedMessage = new String(decryptedBytes);
+
         System.out.println("Расшифрованное сообщение: " + decryptedMessage);
         return decryptedMessage;
 
