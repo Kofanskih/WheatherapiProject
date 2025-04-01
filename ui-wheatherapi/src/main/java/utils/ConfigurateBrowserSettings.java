e73d07535154d42c25a323b4cc010fbd44f17a5c
Kofanskih <kofanskih08@gmail.com>
2025-04-01 21:43:34 +0300

    fix setUpRemote method

--- ui-wheatherapi/src/main/java/utils/ConfigurateBrowserSettings.java
+++ ui-wheatherapi/src/main/java/utils/ConfigurateBrowserSettings.java
@@ -4,6 +4,8 @@ import com.codeborne.selenide.Configuration;
 import org.openqa.selenium.MutableCapabilities;
 
 public class ConfigurateBrowserSettings {
+    public final String URL_PATH = ":4444/wd/hub";
+
     public void setUp(){
         Configuration.browserSize = "1980x1080";
         Configuration.browser = "chrome";
@@ -11,23 +13,9 @@ public class ConfigurateBrowserSettings {
         Configuration.headless = true;
     }
 
-    public void setUpRemoteBrowser(){
-        Configuration.remote = "http://localhost:4444/wd/hub";
+    public void setUpRemote(String url){
+        Configuration.remote = url + URL_PATH;
         Configuration.browserVersion = "127.0";
-        Configuration.browser = "chrome";
-        Configuration.timeout = 5;
-        MutableCapabilities options = new MutableCapabilities();
-        options.setCapability("enableVNC", true);
-        options.setCapability("enableVideo", true);
-        options.setCapability("screenResolution", "1920x1080x24");
-
-        Configuration.browserCapabilities.setCapability("selenoid:options", options);
-
-    }
-
-    public void setUpRemoteServer(){
-        Configuration.remote = "http://134.199.212.192:4444/wd/hub";
-        Configuration.browserVersion = "128.0";
         Configuration.browser = "chrome";
         Configuration.timeout = 5;
         MutableCapabilities options = new MutableCapabilities();
