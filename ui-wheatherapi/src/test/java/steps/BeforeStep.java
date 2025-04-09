5bd48f65e581e5a60645e91ab0e2a730d018d4a0
Kofanskih <kofanskih08@gmail.com>
2025-04-09 16:03:19 +0300

    Update beforeStep

--- ui-wheatherapi/src/test/java/steps/BeforeStep.java
+++ ui-wheatherapi/src/test/java/steps/BeforeStep.java
@@ -3,13 +3,20 @@ package steps;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import io.cucumber.java.en.Given;
+import io.cucumber.testng.CucumberOptions;
 
+
+@CucumberOptions(features = "classpath:ui_weatherapi.src.test.resources.features",
+        glue = {"classpath:ui_weatherapi.src.test.java.steps"},
+        snippets = CucumberOptions.SnippetType.UNDERSCORE)
 public class BeforeStep {
     @Given("Open main page {string}")
     public void open_main_page(String url){
         Configuration.timeout = 60000;
         Selenide.open(url);
+        throw new io.cucumber.java.PendingException();
 
     }
+
 
 }
