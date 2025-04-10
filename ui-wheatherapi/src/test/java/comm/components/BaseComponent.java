872352bda5248b010a9bdb04445afc1a87c4ef4f
Kofanskih <kofanskih08@gmail.com>
2025-04-10 23:26:42 +0300

    Add components

---
+++ ui-wheatherapi/src/test/java/comm/components/BaseComponent.java
@@ -0,0 +1,11 @@
+package comm.components;
+
+import com.microsoft.playwright.Page;
+
+public abstract class BaseComponent {
+    protected Page page;
+
+    protected BaseComponent(final Page page) {
+        this.page = page;
+    }
+}
